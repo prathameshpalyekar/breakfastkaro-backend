d041a6c52d099022e0f35fe073122af95358a377
prathameshpalyekar <pratham2604@gmail.com>
2019-02-25 19:53:39 +0530

    Test commit

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 'use strict';
-
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 export const helloWorld = functions.https.onRequest((request, response) => {
  response.send('Hello from Firebase!\n\n');
