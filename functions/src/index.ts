55ab65e0bce842414b1309692606d6d032581f75
prathameshpalyekar <pratham2604@gmail.com>
2019-02-28 21:27:57 +0530

    Remove complied files

--- functions/src/index.ts
+++
@@ -1,7 +0,0 @@
-'use strict';
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send('Hello from Firebase!\n\n');
-});
