4971e18133f24404100058484a5fa64e04db6c38
prathameshpalyekar <pratham2604@gmail.com>
2019-03-03 18:54:17 +0530

    Add API call for getting user info

--- functions/app/modules/authentication/index.ts
+++ functions/app/modules/authentication/index.ts
@@ -1,14 +1,19 @@
 import * as SignUp from './handlers/SignUp';
 import * as Login from './handlers/Login';
+import * as UserInfo from './handlers/UserInfo';
 
 const routes = [{
     method: 'POST',
     path: '/signup',
     handler: SignUp
 }, {
-    method: 'GET',
+    method: 'POST',
     path: '/login',
     handler: Login
+}, {
+    method: 'POST',
+    path: '/me',
+    handler: UserInfo
 }];
 
 export default routes;
